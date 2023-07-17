449a728231cf45b462dd31779a8da65b750ec7a2
sachinwb <sachinwb1993@gmail.com>
2023-07-18 00:39:10 +0530

    Added:Display All Employee Payroll Details from JSON Object

--- HomePage.js
+++ HomePage.js
@@ -3,21 +3,59 @@ window.addEventListener('DOMContentLoaded',(event) =>{
 });
 
 //Template literal ES6 feature
-const createInnerHtml =()=>{
+const createInnerHtml = () => {
     const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th>"+"<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml =`${headerHtml}
-        <tr>
-            <td><img class ="profile" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\profile-images\Ellipse -2.png" alt="image is missing"></td>
-            <td>Narayan Mahadevan</td>
-            <td>Male</td>
-            <td><div class='dept-label'>HR</div><div class ='dept-label'>Finance</div></td>
-            <td>300000</td>
-            <td>1 Nov 2020</td>
-            <td>
-                <img name="1" onclick="remove(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\delete-black-18dp.svg" alt="delete">
-                <img name="1" onclick="update(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\create-black-18dp.svg" alt="edit">
-            </td>
-        </tr>
-    `;
+    let innerHtml =`${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList){
+        innerHtml =`${innerHtml}
+            <tr>
+                <td><img class ="profile" src="${empPayrollData._profilePic}" alt="image is missing"></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHtml(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
+                <td>
+                    <img name="${empPayrollData._id}" onclick="remove(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\delete-black-18dp.svg" alt="delete">
+                    <img name="${empPayrollData._id}" onclick="update(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\create-black-18dp.svg" alt="edit">
+                </td>
+            </tr>
+        `;
+    }    
     document.querySelector('#table-display').innerHTML =innerHtml;
+}
+
+const getDeptHtml =(deptList) =>{
+    let deptHtml ='';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml}<div class="dept-label">${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = () =>{
+    let employeePayrollListLocal = [
+        {
+            _name: 'Narayan Mahadevan',
+            _gender: 'male',
+            _department:['Engineering','Finance'],
+            _salary:'500000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\profile-images\Ellipse -2.png'
+        },
+        {
+            _name: 'Amarpa Shashanka keerthi kumar',
+            _gender: 'female',
+            _department:['Sales'],
+            _salary:'400000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id:new Date().getTime() +1,
+            _profilePic:'C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\profile-images\Ellipse -1.png'
+        }
+    ];
+    return employeePayrollListLocal;
 }
