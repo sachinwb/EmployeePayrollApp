f1b43efe0afedc7d57c3ac533f3ebcc5704c20f8
sachinwb <sachinwb1993@gmail.com>
2023-07-18 01:01:17 +0530

    Added:UC-6 Display Employee Details from Local Storage

--- HomePage.js
+++ HomePage.js
@@ -1,29 +1,43 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded',(event) =>{
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
 
+const getEmployeePayrollDataFromStorage = () =>{
+    return localStorage.getItem('EmployeePayrollList')?JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
 //Template literal ES6 feature
-const createInnerHtml = () => {
+const createInnerHtml =()=>{
+    
     const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th>"+"<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    if(empPayrollList.length == 0) return;
+
     let innerHtml =`${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
-    for (const empPayrollData of empPayrollList){
-        innerHtml =`${innerHtml}
+
+    // let empPayrollList =createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+        innerHtml=`${innerHtml}    
             <tr>
-                <td><img class ="profile" src="${empPayrollData._profilePic}" alt="image is missing"></td>
+                <td><img class="profile" src="${empPayrollData._profilePic}" alt="Image is missing"></td>
                 <td>${empPayrollData._name}</td>
                 <td>${empPayrollData._gender}</td>
                 <td>${getDeptHtml(empPayrollData._department)}</td>
                 <td>${empPayrollData._salary}</td>
-                <td>${empPayrollData._startDate}</td>
+                <td>${stringifyDate(empPayrollData._startDate)}</td>
                 <td>
-                    <img name="${empPayrollData._id}" onclick="remove(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\delete-black-18dp.svg" alt="delete">
-                    <img name="${empPayrollData._id}" onclick="update(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\create-black-18dp.svg" alt="edit">
+                    <img id="${empPayrollData._id}" onclick="remove(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\delete-black-18dp.svg" alt="delete">
+                    <img id="${empPayrollData._id}" onclick="update(this)" src="C:\Users\sachi\Downloads\FS HTML_CSS LP02 Employee Payroll App Assets\assets\icons\create-black-18dp.svg" alt="edit">
                 </td>
             </tr>
         `;
-    }    
-    document.querySelector('#table-display').innerHTML =innerHtml;
+    }
+    
+    document.querySelector('#table-display').innerHTML=innerHtml;
 }
 
 const getDeptHtml =(deptList) =>{
